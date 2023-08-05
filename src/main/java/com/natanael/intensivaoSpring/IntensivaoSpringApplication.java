e7cf522955cfc30fdff5d4b01f8a8a3fad8e5c73
Your Name <you@example.com>
2023-08-05 10:43:08 -0300

    Initial commit

---
+++ src/main/java/com/natanael/intensivaoSpring/IntensivaoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.natanael.intensivaoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivaoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivaoSpringApplication.class, args);
+	}
+	//57:11 video
+}
---
+++ src/test/java/com/natanael/intensivaoSpring/IntensivaoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natanael.intensivaoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivaoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
